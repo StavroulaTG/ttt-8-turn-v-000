9998ae05eace460a4b5a035c237f3a327fe77333
Stavroula Guerrero <4stacyg@optonline.net>
2017-12-08 02:05:02 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,38 @@
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, value)
+    board[index] = value
+end
+
+def position_taken?(board, index)
+  board[index] != " "
+end
+
+def valid_move?(board, position)
+  if position.to_i.between?(1,9)
+    if !position_taken?(board, position.to_i-1)
+      true
+    end
+  end
+
+  def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  if valid_move?(board, index)
+    move(board, input)
+  else
+    turn(board)
+  end
+  display_board(board)
+end
