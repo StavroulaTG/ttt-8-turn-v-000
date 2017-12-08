b714165635cfa1fdd453ae45b5996d91a1f087fd
Stavroula Guerrero <4stacyg@optonline.net>
2017-12-08 02:30:54 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,27 +12,32 @@ def input_to_index(user_input)
 end
 
 def move(board, index, current_player)
-    board[index] = current_player
+  board[index] = current_player
 end
 
 def position_taken?(board, location)
-  board[location] != " " && board[location] != " "
+  board[location] != " " && board[location] != ""
 end
 
 def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
 
-
 def turn(board)
   current_player = current_player(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-
   if valid_move?(board, index) == false
     turn(board)
   elsif valid_move?(board, index) == true
-
   end
+    move(board, index, current_player)
+    display_board(board)
 end
+
+
+
+
+
+
